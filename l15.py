8884dcc06b0d3c0bf2b095243e93d895038906db
Marcin Król <szeryfmk@gmail.com>
2020-12-19 17:57:32 +0100

    second commit, after lesson 15 on udemy...

---
+++ l15.py
@@ -0,0 +1,15 @@
+# Rzutowanie standardowo pobieranego przez Pythona srtinga na integer(l. całkowite)
+print("Kalkulator, który dodaje liczby integerowe.")
+print("Podaj liczbę a: ")
+a = int(input())
+print("Podaj liczbę b: ")
+b = int(input())
+print("Podane przez Ciebie liczby a:", a, "plus b:", b, "to się równa:", a + b)
+
+# zad: Jak masz na imię i ile masz lat
+print("\nPodaj swoje imię:")
+name = input()
+print("Na imię masz:", name, "\nPodaj ile masz lat:")
+age = int(input())
+age += 1
+print("Witaj", name, "za rok będziesz miał lat:", age)
