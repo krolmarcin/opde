ae37deca1c03eb6a1e5c81d3222e416601bc2202
Marcin Król <szeryfmk@gmail.com>
2020-12-29 13:08:38 +0100

    lesson 16 commit, and added a = int(input()), that I mean projection in polish rzutowanie

--- l16.py
+++ l16.py
@@ -18,6 +18,10 @@ print(2 == 2)
 # przypisanie do a inputa jako striga i porówanie do integera
 print("Podaj liczbę 5:")
 a = input()
-print("nie print(5 = 5) tylko print(5 == 5), print(a == 5) zwróci False, bo = to operator przypsania, a == to porównanie. "
-      "Żeby było True w przypdaku print(5 == 5) to trzeba dać: a = int(input())")
+print("Nie print(5 = 5) tylko print(5 == 5), print(a == 5) zwróci False, bo = to operator przypsania, a == to porównanie.\n"
+      "Żeby było True w przypdaku print(5 == 5) to trzeba dać: a = int(input()). Inaczej wynik to:")
+print(a == 5)
+print("Podaj liczbę 5:")
+a = int(input())
+print("Teraz wynik da True, bo zastosowałem 'a = int(input())':")
 print(a == 5)
