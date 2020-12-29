0cb18945914c21c966252d89128210b290b140d2
Marcin Król <szeryfmk@gmail.com>
2020-12-29 19:28:09 +0100

    lesson18

---
+++ l18.py
@@ -0,0 +1,7 @@
+#wartość rózna od 0 w pythonie jest true
+a = int(input("Podaj liczbę stałoprzecinkową, może być ujemna: \n"))
+
+if (a):
+    print("podałeś/aś liczbę różną od 0")
+else:
+    print("podałeś/aś wartość 0")
