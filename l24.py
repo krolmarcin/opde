8494e007fcdebbaebb8c71d7992c5e13274aeddf
Marcin Król <szeryfmk@gmail.com>
2021-01-04 17:57:54 +0100

    lesson 24 + zad

--- l24.py
+++ l24.py
@@ -3,8 +3,18 @@
 wynik = 0
 i = 0
 
-for i in range(0, 4):
+for i in range(4):
     x = int(input("Podaj liczbę: "))
     wynik += x
 
 print("Wynik dodawania liczb, które podałeś to:", wynik)
+
+i = 0
+for i in range(15):
+    if (i % 2 == 0):
+        print("Liczba", i, " jest parzysta")
+
+l = 0
+for l in range(201):
+    if (l % 5 == 0 and l % 7 != 0):
+        print(l)
