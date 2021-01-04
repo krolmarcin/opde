dc38d675ee5ee71a7455c347fb28905e972a6634
Marcin Król <szeryfmk@gmail.com>
2021-01-04 17:46:15 +0100

    lesson 24 corrected in range() without i += 1

--- l24.py
+++ l24.py
@@ -1,10 +1,10 @@
 # Petla for
+# i in range(5), i in 'trala', i n [0, 1, 2, 3],
 wynik = 0
 i = 0
 
 for i in range(0, 4):
     x = int(input("Podaj liczbę: "))
     wynik += x
-    i += 1
 
 print("Wynik dodawania liczb, które podałeś to:", wynik)
