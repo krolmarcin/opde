3f1d447ac29eab175856e829db8ef361752b579c
Marcin Król <szeryfmk@gmail.com>
2021-01-04 17:37:04 +0100

    lesson 23 and 24 + debugging

---
+++ l24.py
@@ -0,0 +1,10 @@
+# Petla for
+wynik = 0
+i = 0
+
+for i in range (0, 4):
+    x = int(input("Podaj liczbę: "))
+    wynik += x
+    i += 1
+
+print("Wynik dodawania liczb, które podałeś to:", wynik)
